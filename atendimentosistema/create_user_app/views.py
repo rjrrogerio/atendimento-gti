f4dd90f49f78a2349cbed6cfd5ae0934d23c3f48
Rogerio VIEIRA <rjr.rogerio@gmail.com>
2023-07-23 23:02:03 -0300

    adicionado hide/show no campo dataContrato

--- atendimentosistema/create_user_app/views.py
+++ atendimentosistema/create_user_app/views.py
@@ -4,7 +4,7 @@ import string
 import random
 
 
-def id_generator(size=10, chars=string.ascii_uppercase + string.digits):
+def id_generator(size=10, chars=string.ascii_uppercase+string.ascii_lowercase + string.digits):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def createUser(request):
@@ -20,36 +20,37 @@ def createUserTest(request):
 def createScript(request):
     nome = []
     email = []
-    uo    = []
+    uo = []
     tipo = []
     licenca = []
     datacontrato = []
     senha = [] 
+    file_data = []
 
     context = {}
     if request.method == "POST":
         countField = int(request.POST.get('countField'))
-        if countField:
-            for i in range(countField):
-                nome.append(request.POST.get('field_nome[{}]'.format(i)))
-                email.append(request.POST.get('field_email[{}]'.format(i)))
-                uo.append(request.POST.get('field_uo[{}]'.format(i)))
-                tipo.append(request.POST.get('field_tipo[{}]'.format(i)))
-                licenca.append(request.POST.get('field_licenca[{}]'.format(i)))
-                datacontrato.append(request.POST.get('field_datacontrato[{}]'.format(i)))
-                senha.append(id_generator())
-            
-            print(senha)
+        for i in range(countField):
+            nome.append(request.POST.get('field_nome[{}]'.format(i)))
+            email.append(request.POST.get('field_email[{}]'.format(i)))
+            uo.append(request.POST.get('field_uo[{}]'.format(i)))
+            tipo.append(request.POST.get('field_tipo[{}]'.format(i)))
+            licenca.append(request.POST.get('field_licenca[{}]'.format(i)))
+            datacontrato.append(request.POST.get('field_datacontrato[{}]'.format(i)))
+            senha.append(id_generator())
+            file_data.append('New-ADUser -Name "{}" -GivenName "{}" -Surname "{}" -SamAccountName "{}" -UserPrincipalName "{}" -Path "OU=Users,OU=Accounts,OU=Berlin,OU=DE,DC=woshub,DC=com";'.format(nome[i],email[i],uo[i],tipo[i],licenca[i]))
 
-            fullname = request.POST.get('fullname')
-            email = request.POST.get('email')
-            numberunity = request.POST.get('numberunity')
-            typeemploye = request.POST.get('typeemploye')
-            typelicense = request.POST.get('typelicense')
-            file_data = 'New-ADUser -Name "{}" -GivenName "{}" -Surname "{}" -SamAccountName "{}" -UserPrincipalName "{}" -Path "OU=Users,OU=Accounts,OU=Berlin,OU=DE,DC=woshub,DC=com" -AccountPassword(Read-Host -AsSecureString "Input Password")Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book.'.format(fullname,fullname,fullname,email,email)
+        print(nome)
+        print(email)
+        print(uo)
+        print(tipo)
+        print(licenca)
+        print(datacontrato)
+        print(senha)
 
-            response = HttpResponse(file_data, content_type='application/text charset=utf-8')
-            response['Content-Disposition'] = 'attachment; filename="script.ps1"'
-            return response
+         
+        response = HttpResponse(file_data, content_type='application/text charset=utf-8')
+        response['Content-Disposition'] = 'attachment; filename="script.txt"'
+        return response
 
     return render(request, 'create_user_app/create_user_home_test.html', context)
--- atendimentosistema/static/custom/js/custom.js
+++ atendimentosistema/static/custom/js/custom.js
@@ -1,30 +1,20 @@
 $(function () {
-  /*
-  $('#tipodefuncionario').on('change', function (e) {
-    var optionSelected = $("option:selected", this);
+  
+  $("body").on("change",'#tipodefuncionario', function (e) {
     var valueSelected = this.value;
-    console.log(valueSelected)
-    if (valueSelected == 3) {
-      $(".licenca").hide();
-
+    var nameField = this.name;
+    var numberField = nameField.replace(/[^0-9]/g, '');
+    
+    if (valueSelected == "funcionario") {
+      console.log(numberField)
+      document.getElementById('datacontrato['+numberField+']').style.display = "block";
     } else {
-      $(".licenca").show();
+      console.log(numberField)  
+      document.getElementById('datacontrato['+numberField+']').style.display = "none";
     }
-  });
+  })
 
-  $('#tipodefuncionario').on('change', function (e) {
-    var optionSelected = $("option:selected", this);
-    var valueSelected = this.value;
-    console.log(valueSelected)
-    if (valueSelected == 1) {
-      $(".datacontrato").hide();
-
-    } else {
-      $(".datacontrato").show();
-    }
-  });*/
-
-  var maxField = 10; //Input fields increment limitation
+  var maxField = 20; //Input fields increment limitation
   var addButton = $('.add_button'); //Add button selector
   var wrapper = $('.field_wrapper'); //Input field wrapper
   var x = 1; //Initial field counter is 1
@@ -34,7 +24,7 @@ $(function () {
   $(addButton).click(function () {
     //Check maximum number of input fields
     if (x < maxField) {
-      var fieldHTML = '<div class="row"> <div class="col-3 m-1"> <input class="form-control " type="text" name="field_nome['+x+']" value="" placeholder="Nome completo" /> </div> <div class="col-2 m-1"> <input class="form-control " type="text" name="field_email['+x+']" value="" placeholder="nome@sescsp.org.br" /> </div> <div class="col-1 m-1"> <input class="form-control " type="text" name="field_uo['+x+']" value="" placeholder="UO" /> </div> <div class="col-2 m-1"> <select class="form-control" name="field_tipo['+x+']" aria-label=".form-select-lg example" id="tipodefuncionario"> <option selected value="funcionario">Funcionário</option> <option value="estagiario">Estagiário</option><option value="pj">PJ</option> <option value="temporario">Temporário</option> </select> </div> <div class="col-1 m-1"> <select class="form-control" name="field_licenca['+x+']" aria-label=".form-select-sm example" id="tipodelicenca"> <option selected value="lica1">LIC-A1</option> <option value="lica3" name="lica3">LIC-A3</option> </select> </div> <div class="col-2 m-1 datacontrato"> <input class="form-control" type="date" name="field_datacontrato['+x+']" placeholder="Data do contrato"> </div> </div>'; //New input field html 
+      var fieldHTML = '<div class="row"> <div class="col-3 m-1"> <input class="form-control " type="text" name="field_nome['+x+']" value="" placeholder="Nome completo" /> </div> <div class="col-2 m-1"> <input class="form-control " type="text" name="field_email['+x+']" value="" placeholder="nome@sescsp.org.br" /> </div> <div class="col-1 m-1"> <input class="form-control " type="text" name="field_uo['+x+']" value="" placeholder="UO" /> </div> <div class="col-2 m-1"> <select class="form-control" name="field_tipo['+x+']" aria-label=".form-select-lg example" id="tipodefuncionario"> <option selected value="funcionario">Funcionário</option> <option value="estagiario">Estagiário</option><option value="pj">PJ</option> <option value="temporario">Temporário</option> </select> </div> <div class="col-1 m-1"> <select class="form-control" name="field_licenca['+x+']" aria-label=".form-select-sm example" id="tipodelicenca"> <option selected value="lica1">LIC-A1</option> <option value="lica3" name="lica3">LIC-A3</option> </select> </div> <div class="col-2 m-1" id="datacontrato['+x+']"> <input class="form-control" id="datacontrato" type="date" name="field_datacontrato['+x+']" placeholder="Data do contrato"> </div> </div>'; //New input field html 
       x++; //Increment field counter
       $(wrapper).append(fieldHTML); //Add field html
       document.getElementById('countField').value = x
