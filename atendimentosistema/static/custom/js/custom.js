ea9e1ad0b5743d381cf4e243b35f6b1a792d3a0c
Rogerio <rjr.rogerio@gmail.com>
2023-08-18 17:57:16 -0300

    fix

--- atendimentosistema/static/custom/js/custom.js
+++ atendimentosistema/static/custom/js/custom.js
@@ -17,15 +17,24 @@ $(function () {
   var removeButton = $('.remove_button');
   var wrapper = $('.field_wrapper');
   var x = 1; //Initial field counter is 1
-
   //when add button is clicked
-  $(addButton).click(function () {
+  $(addButton).click(function () {    
     if (x < maxField) {
-      var fieldHTML = '<div class="row line' + x + '" > <div class="col-3 m-1"> <input class="form-control " type="text" name="field_nome[' + x + ']" value="" placeholder="Nome completo" required="required"/> </div> <div class="col-2 m-1"> <input class="form-control " type="text" name="field_email[' + x + ']" value="" placeholder="nome.sobrenome" required="required"/> </div><div class="col-1 m-1"><select class="form-control"  name="field_uo[' + x + ']" id="field_uo"></select></div><div class="col-2 m-1"> <select class="form-control" name="field_tipo[' + x + ']" aria-label=".form-select-lg example" id="tipodefuncionario"> <option selected value="funcionario">Funcionário</option> <option value="estagiario">Estagiário</option><option value="pj">PJ</option> <option value="temporario">Temporário</option> </select> </div> <div class="col-1 m-1"> <select class="form-control" name="field_licenca[' + x + ']" aria-label=".form-select-sm example" id="tipodelicenca"> <option selected value="lica1">LIC-A1</option> <option value="lica3" name="lica3">LIC-A3</option> </select> </div> <div class="col-2 m-1" id="datacontrato[' + x + ']" style="display: none"> <input class="form-control" id="datacontrato" type="date" name="field_datacontrato[' + x + ']" placeholder="Data do contrato"> </div> </div>';
+      var fieldHTML = '<div class="row line' + x + '" > <div class="col-3 m-1"> <input class="form-control " type="text" name="field_nome[' + x + ']" value="" placeholder="Nome completo" required="required"/> </div> <div class="col-2 m-1" id="id_field_email"> <input class="form-control" type="text" id="field_email" name="field_email[' + x + ']" value="" placeholder="nome.sobrenome" required="required"/> </div><div class="col-2 m-1"> <select class="form-control" name="field_tipo[' + x + ']" aria-label=".form-select-lg example" id="tipodefuncionario"> <option selected value="funcionario">Funcionário</option> <option value="estagiario">Estagiário</option><option value="pj">PJ</option> <option value="temporario">Temporário</option> </select> </div> <div class="col-1 m-1"> <select class="form-control" name="field_licenca[' + x + ']" aria-label=".form-select-sm example" id="tipodelicenca"> <option selected value="lica1">LIC-A1</option> <option value="lica3" name="lica3">LIC-A3</option> </select> </div> <div class="col-2 m-1" id="datacontrato[' + x + ']" style="display: none"> <input class="form-control" id="datacontrato" type="date" name="field_datacontrato[' + x + ']" placeholder="Data do contrato"> </div> </div>';
+      
+      
       $(wrapper).append(fieldHTML);
-      x++;
+      $('#id_field_uo')
+      .clone()
+      .attr('id', 'field_uo-'+ x++)
+      .insertAfter($('[id^=id_field_email]:last'));
       document.getElementById('countField').value = x
-      $("#field_uo").html( $("#field_uo_first").html() );
+      
+      document.getElementById('field_uo').setAttribute('name', 'field_uo['+x+']');
+      document.getElementById('field_uo').setAttribute('id', 'used');
+      
+      
+      
     }
   });
   $(removeButton).click(function () {
