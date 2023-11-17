2b1849ad9512c16990166f8059b4fd80ca6cb2f5
Rogerio <rjr.rogerio@gmail.com>
2023-11-17 10:09:14 -0300

    Remove ignored files

--- atendimentosistema/create_user_app/utils/create_log.py
+++
@@ -1,9 +0,0 @@
-from ..models import Transacao
-
-def save_log(usuario, data, sistema_utilizado, login_alterado):
-    log =  Transacao()
-    log.usuario = usuario
-    log.data = data
-    log.sistemaUtilizado = sistema_utilizado
-    log.loginAlterado = login_alterado
-    log.save()
--- atendimentosistema/create_user_app/utils/script_add_grupo.py
+++
@@ -1,5 +0,0 @@
-
-def get_data_script_group(dados_script,nome_logon_base,nome_logon_destino):
-
-    dados_script.append('$nomeUsuario1 = "{nome_logon_base}";\n$nomeUsuario2 = "{nome_logon_destino}";\n$gruposUsuario1 = Get-ADPrincipalGroupMembership -Identity $nomeUsuario1 | Select-Object -ExpandProperty Name;\n$gruposUsuario2 = Get-ADPrincipalGroupMembership -Identity $nomeUsuario2 | Select-Object -ExpandProperty Name;\n$gruposApenasNoUsuario1 = $gruposUsuario1 | Where-Object {{ $_ -notin $gruposUsuario2 }};\n$gruposAdicionados = @();\nforeach ($grupo in $gruposApenasNoUsuario1) {{\n   Add-ADGroupMember -Identity $grupo -Members $nomeUsuario2;\n   $gruposAdicionados += $grupo;}}\n if ($gruposAdicionados.Count -gt 0) {{ \n   Write-Host "Grupos adicionados ao usuário " $nomeUsuario2; \n   $gruposAdicionados | ForEach-Object {{ Write-Host $_ }}}} else {{ \n   Write-Host "Nenhum grupo foi adicionado ao usuário $nomeUsuario2."}} \n'.format(nome_logon_base = nome_logon_base, nome_logon_destino = nome_logon_destino))                       
-    return dados_script
